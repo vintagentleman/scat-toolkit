d7fb54fa4ccffb8c0dafb438f75bfd58b47863d6
Konstantin Sipunin <79818514079@yandex.ru>
2021-10-17 11:08:52 +0400

    Rewrite tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,16 +1,17 @@
 import re
 from pathlib import Path
+from typing import List, Tuple
 
-from fire import Fire
+import click
 
+from models.number import Number
+from models.row import Row, WordRow
 from src import __root__
-from _models import Row
 from utils import replace_chars
-from utils.number import Number
+from utils.characters import cyrillic_homoglyphs, latin_homoglyphs
 
 
-def parse_line(line):
-    # line = replace_chars(line, "ABEKMHOPCTXЭaeopcyx", "АВЕКМНОРСТХ+аеорсух")
+def parse_line(manuscript_id: str, line: str) -> Tuple[List[Row], List[str]]:
     nums = line[line.rfind("/") + 1 :].split()
     toks = list(
         filter(bool, re.split(r'(</.+?>|<[a-z].+?">)|\s+', line[: line.rfind("/")]))
@@ -18,9 +19,7 @@ def parse_line(line):
 
     for i in range(len(toks)):
         if not (toks[i].startswith("</") or toks[i].endswith('">')):
-            toks[i] = replace_chars(
-                toks[i], "ABEKMHOPCTXЭaeopcyx", "АВЕКМНОРСТХ+аеорсух"
-            )
+            toks[i] = replace_chars(toks[i], latin_homoglyphs, cyrillic_homoglyphs)
 
     i = 0
 
@@ -60,33 +59,47 @@ def parse_line(line):
 
         i += 1
 
-    return [Row([t, None]) for t in toks], nums
+    # TODO Account for XML rows
+    return [WordRow(manuscript_id, [t] + [""] * 6) for t in toks], nums
 
 
-def generate_token(toks, nums):
+def generate_token(rows, nums):
     nums_done = 0
 
-    for t in toks:
-        if t.word == "*":
+    for row in rows:
+        if row.source == "*":
             num = Number(nums[nums_done])
-            yield ("{}\t{}" + "\t" * 5).format(str(t).replace("*", num.text), num.value)
+            yield f"{row.columns[0].replace('*', num.text)}\t{num.value}\t\t\t\t\t"
             nums_done += 1
         else:
-            yield str(t) + "\t" * 6
+            yield row.tsv()
 
 
-def run(inp="*.txt", enc="IBM866"):
-    filenames = Path.joinpath(__root__, "inp", "tokenizer").glob(inp)
+@click.command()
+@click.option(
+    "--path",
+    "-p",
+    type=click.Path(),
+    default="raw",
+    help="Path to content files, relative to `scat-content` submodule path",
+)
+@click.option("--encoding", "-e", default="IBM866")
+@click.argument("glob", default="*.txt")
+def main(path: str, encoding: str, glob: str):
+    filepaths = list(Path.joinpath(__root__, "scat-content", path).glob(glob))
 
-    for filename in filenames:
-        with open(filename, mode="r", encoding=enc) as raw, Path.joinpath(
-            __root__, "out", filename.stem + ".tsv"
+    Path.joinpath(__root__, "generated").mkdir(exist_ok=True)
+    Path.joinpath(__root__, "generated", "tokenizer").mkdir(exist_ok=True)
+
+    for filepath in filepaths:
+        with open(filepath, mode="r", encoding=encoding) as raw, Path.joinpath(
+            __root__, "generated", "tokenizer", f"{filepath.stem}.tsv"
         ).open(mode="w", encoding="utf-8") as tokenized:
             for line in raw.readlines():
-                parsed = parse_line(line)
+                parsed = parse_line(filepath.stem, line)
                 for token in generate_token(*parsed):
-                    tokenized.write(token + "\n")
+                    tokenized.write(f"{token}\n")
 
 
 if __name__ == "__main__":
-    Fire(run)
+    main()

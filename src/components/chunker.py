f504891a26f9684f26f2db218c222bdf7c11c66d
Konstantin Sipunin <79818514079@yandex.ru>
2023-10-28 19:16:04 +0600

    Add some chunking rules

--- src/components/chunker.py
+++ src/components/chunker.py
@@ -12,12 +12,32 @@ class Chunker:
         }
 
     @staticmethod
-    def __has_punctuation_before_conjunction(row: Row, next_row: Row) -> bool:
+    def __is_followed_by_i_after_strongish_punctuation(row: Row, next_row: Row) -> bool:
         return (
             row.tail_punctuation is not None
             and set(row.tail_punctuation.source) & {".", ":", ";"}
             and next_row.word is not None
-            and next_row.word.lemma in ("И", "ИЖЕ")
+            and next_row.word.lemma == "и"
+        )
+
+    @staticmethod
+    def __is_followed_by_relative_conjunction_after_punctuation(
+        row: Row, next_row: Row
+    ) -> bool:
+        return (
+            row.tail_punctuation is not None
+            and next_row.word is not None
+            and next_row.word.lemma in ("иже", "еже")
+        )
+
+    @staticmethod
+    def __is_followed_by_punctuation_and_imperative(row: Row, next_row: Row) -> bool:
+        return (
+            row.tail_punctuation is not None
+            and next_row.word is not None
+            and next_row.word.tagset is not None
+            and next_row.word.pos.startswith("гл")
+            and next_row.word.tagset.mood == "повел"
         )
 
     @classmethod
@@ -30,7 +50,13 @@ class Chunker:
 
             if cls.__has_strong_punctuation(row) or (
                 i < len(rows) - 1
-                and cls.__has_punctuation_before_conjunction(row, rows[i + 1])
+                and (
+                    cls.__is_followed_by_i_after_strongish_punctuation(row, rows[i + 1])
+                    or cls.__is_followed_by_relative_conjunction_after_punctuation(
+                        row, rows[i + 1]
+                    )
+                    or cls.__is_followed_by_punctuation_and_imperative(row, rows[i + 1])
+                )
             ):
                 chunks.append(chunk)
                 chunk = []
